b2e355c5a316da8ffe7c08652520caca86eb491c
BrennanG01 <brennan@imgoins.com>
2023-05-16 10:22:21 -0700

    Update portfolio implementation in order to display page contents.

--- src/data/portfolio.tsx
+++ src/data/portfolio.tsx
@@ -2,19 +2,19 @@ export default [
     {
         title: 'Direct Campaign',
         imgUrl: 'https://th.bing.com/th/id/OIP.T6F6Vja5ue4BhRs81wWlywHaHa?pid=ImgDet&rs=1',
-        stack: ['React', 'TypeScript', 'Agile Programming'],
-        link: 'www.google.com',
+        stack: ['React ', 'TypeScript ', 'Agile Programming'],
+        link: 'www.directcampaign.org',
     },
     {
         title: 'Direct Campaign',
         imgUrl: 'https://th.bing.com/th/id/OIP.T6F6Vja5ue4BhRs81wWlywHaHa?pid=ImgDet&rs=1',
-        stack: ['React', 'TypeScript', 'Agile Programming'],
-        link: 'www.google.com',
+        stack: ['React ', 'TypeScript ', 'Agile Programming'],
+        link: 'www.directcampaign.org',
     },
     {
         title: 'Direct Campaign',
         imgUrl: 'https://th.bing.com/th/id/OIP.T6F6Vja5ue4BhRs81wWlywHaHa?pid=ImgDet&rs=1',
-        stack: ['React', 'TypeScript', 'Agile Programming'],
-        link: 'www.google.com',
+        stack: ['React ', 'TypeScript ', 'Agile Programming'],
+        link: 'www.directcampaign.org',
     },
 ]
