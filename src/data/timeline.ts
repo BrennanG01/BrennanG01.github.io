bcd8620e97daf199898545fa130ec0f44b7ba67b
BrennanG01 <brennan@imgoins.com>
2023-05-17 07:23:59 -0700

    Implement timeline with sample data

---
+++ src/data/timeline.ts
@@ -0,0 +1,20 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Direct Campaign',
+        duration: '2 years',
+        details: 'Identified opportunity for improvement in the political campaign management industry via software development.',
+    },
+    {
+        year: 'Today',
+        title: 'Direct Campaign',
+        duration: '2 years',
+        details: 'Identified opportunity for improvement in the political campaign management industry via software development.',
+    },
+    {
+        year: 'Today',
+        title: 'Direct Campaign',
+        duration: '2 years',
+        details: 'Identified opportunity for improvement in the political campaign management industry via software development.',
+    },
+]
