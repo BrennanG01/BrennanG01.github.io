9dfddacf94b26e8d8305c54863c8662aba217918
BrennanG01 <brennan@imgoins.com>
2023-05-17 08:38:12 -0700

    Implement footer component.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,10 @@
 
 function Footer() {
     return (
-        <div>
-            <p>Footer</p>
+        <div className="py-5 text-center">
+            <p className="text-sm mt-2 opacity-50">
+                &copy; {new Date().getFullYear()} Brennan Goins. All rights reserved.
+            </p>
         </div>
     )
 }
